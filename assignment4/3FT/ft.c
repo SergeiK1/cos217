8b0d88087b6561c85c160175d02da26973a00503
Joshua Song <joshua.song@princeton.edu>
2026-04-08 15:10:01 -0400

    comment

--- assignment4/3FT/ft.c
+++ assignment4/3FT/ft.c
@@ -120,7 +120,7 @@ static int FT_traversePath(Path_T oPPath, Node_T *poNFurthest) {
   Traverses the FT to find a node with absolute path pcPath. Returns a
   int SUCCESS status and sets *poNResult to be the node, if found.
   Otherwise, sets *poNResult to NULL and returns with status:
-  * INITIALIZATION_ERROR if the DT is not in an initialized state
+  * INITIALIZATION_ERROR if the FT is not in an initialized state
   * BAD_PATH if pcPath does not represent a well-formatted path
   * CONFLICTING_PATH if the root's path is not a prefix of pcPath
   * NO_SUCH_PATH if no node with pcPath exists in the hierarchy
@@ -281,7 +281,7 @@ int FT_insertDir(const char *pcPath) {
    }
 
    Path_free(oPPath);
-   /* update DT state variables to reflect insertion */
+   /* update FT state variables to reflect insertion */
    if(oNRoot == NULL)
       oNRoot = oNFirstNew;
    ulCount += ulNewNodes;
