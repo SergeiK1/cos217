070dacd2767ca143efd7daaaf385aeb629178625
SergeiK1 <sergeikdr1@gmail.com>
2026-04-20 19:14:28 -0400

    part 2d done

--- assignment5/bigintadd.s
+++ assignment5/bigintadd.s
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------*/
-/* mywc.s                                                             */
+/* bigintadd.s                                                             */
 /* Author: Joshua (Kimyung) Song, Sergei Kudriavtcev                   */
 /*--------------------------------------------------------------------*/
 
@@ -37,6 +37,8 @@
     .global BigInt_add
 
 
+// ----------------------------------------
+
 BigInt_larger:
     // Prolog
     sub     sp, sp, LARGER_STACK_BYTECOUNT
@@ -64,4 +66,195 @@ endif1:
     ldr     x30, [sp]
     add     sp, sp, LARGER_STACK_BYTECOUNT
     ret
+
+
+// ----------------------------------------
+
+BigInt_add: 
+    // Prolog
+    sub     sp, sp, ADD_STACK_BYTECOUNT
+    str     x30, [sp]
+    str     x0, [sp, OADDEND1]
+    str     x1, [sp, OADDEND2]
+    str     x2, [sp, OSUM]
+
+
+
+    // Run the BigInt_larger function
+    ldr     x0, [sp, OADDEND1] // load pointer
+    ldr     x0, [x0, LLENGTH] // load the value 
+
+    ldr     x1, [sp, OADDEND2] 
+    ldr     x1, [x1, LLENGTH]
+
+    bl      BigInt_larger
+
+
+    // Store returning value 
+    str     x0, [sp, LSUMLENGTH]
+
+
+    // oSum Length if-statement
+    ldr     x1, [sp, OSUM]
+    ldr     x1, [x1, LLENGTH]
+
+    cmp     x1, x0
+    ble     endifClear
+
+
+    // x0 = oSum->aulDigits 
+    ldr     x0, [sp, OSUM]
+    add     x0, x0, AULDIGITS
+
+    // x1 = 0
+    mov     x1, 0
+
+    // x2 = MAXX_DIGITS * sizeof(unsigned long) = 32768 * 8
+    mov     x2, MAX_DIGITS
+    lsl     x2, x2, 3 // *8 
+    
+    bl memset // already exists
+
+endifClear:
+
+    // ulCarry = 0
+    mov     x0, 0
+    str     x0, [sp, ULCARRY]
+
+    // ulIndex = 0
+    mov     x0, 0
+    str     x0, [sp, LINDEX]
+
+forloop1:
+
+    // if (lIndex >= lSumLenght) goto endforloop1
+    ldr     x0, [sp, LINDEX]
+    ldr     x1, [sp, LSUMLENGTH]
+    cmp     x0, x1
+    bge     endforloop1
+
+    // ulSum = ulCarry
+    ldr     x0, [sp, ULCARRY]
+    str     x0, [sp, ULSUM]
+
+    mov     x0, 0
+    str     x0, [sp, ULCARRY]
+
+    // ulSum += oAdden1->aulDigits[lIndex];
+    ldr     x0, [sp, OADDEND1]
+    add     x0, x0, AULDIGITS
+    ldr     x1, [sp, LINDEX]
+    ldr     x2, [x0, x1, lsl 3]
+    
+    ldr     x0, [sp, ULSUM]
+    add     x0, x0, x2
+    str     x0, [sp, ULSUM]
+
+    // if (ulSum >= oAdden1->aulDiggits[lIndex]) 
+    // goto endifCarry1;
+    ldr     x0, [sp, OADDEND1]
+    add     x0, x0, AULDIGITS
+    ldr     x1, [sp, LINDEX]
+    ldr     x2, [x0, x1, lsl 3]
+
+    ldr     x0, [sp, ULSUM]
+    cmp     x0, x2
+    bhs     endifCarry1
+
+    // ulCarry = 1;
+    mov     x0, 1
+    str     x0, [sp, ULCARRY]
+
+endifCarry1:
+    // ulSum += oAddend2->aulDigits[lIndex]
+    ldr     x0, [sp, OADDEND2]
+    add     x0, x0, AULDIGITS
+    ldr     x1, [sp, LINDEX]
+    ldr     x2, [x0, x1, lsl 3]
+
+    ldr     x0, [sp, ULSUM]
+    add     x0, x0, x2
+    str     x0, [sp, ULSUM]
+
+     // if (ulSum >= oAddend2->aulDigits[lIndex]) goto endifCarry2
+    ldr     x0, [sp, OADDEND2]
+    add     x0, x0, AULDIGITS
+    ldr     x1, [sp, LINDEX]
+    ldr     x2, [x0, x1, lsl 3]
+
+    ldr     x0, [sp, ULSUM]
+    cmp     x0, x2
+    bhs     endifCarry2
+
+    // ulCarry = 1
+    mov     x0, 1
+    str     x0, [sp, ULCARRY]
+
+endifCarry2:
+
+    // oSum->aulDigits[lIndex] = ulSum
+    ldr     x0, [sp, OSUM]
+    add     x0, x0, AULDIGITS
+    ldr     x1, [sp, LINDEX]
+    ldr     x2, [sp, ULSUM]
+    str     x2, [x0, x1, lsl 3]
+
+    // lIndex++
+    ldr     x0, [sp, LINDEX]
+    add     x0, x0, 1
+    str     x0, [sp, LINDEX]
+
+    b       forloop1
+
+endforloop1:
+
+    // if (ulCarry != 1) goto endifCarry3
+    ldr     x0, [sp, ULCARRY]
+    cmp     x0, 1
+    bne     endifCarry3
+
+    // if (lSumLength != MAX_DIGITS) goto endifFalse
+    ldr     x0, [sp, LSUMLENGTH]
+    mov     x1, MAX_DIGITS
+    cmp     x0, x1
+    bne     endifFalse
+
+    // return FALSE
+    mov     w0, FALSE
+    ldr     x30, [sp]
+    add     sp, sp, ADD_STACK_BYTECOUNT
+    ret
+
+endifFalse:
+
+    // oSum->aulDigits[lSumLength] = 1
+    ldr     x0, [sp, OSUM]
+    add     x0, x0, AULDIGITS
+    ldr     x1, [sp, LSUMLENGTH]
+    mov     x2, 1
+    str     x2, [x0, x1, lsl 3]
+
+    // lSumLength++
+    ldr     x0, [sp, LSUMLENGTH]
+    add     x0, x0, 1
+    str     x0, [sp, LSUMLENGTH]
+
+endifCarry3:
+
+    // oSum->lLength = lSumLength
+    ldr     x0, [sp, OSUM]
+    ldr     x1, [sp, LSUMLENGTH]
+    str     x1, [x0, LLENGTH]
+
+    // return TRUE
+    mov     w0, TRUE
+    ldr     x30, [sp]
+    add     sp, sp, ADD_STACK_BYTECOUNT
+    ret
+
+    
+
+    
+    
+    
 
