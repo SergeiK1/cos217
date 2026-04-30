5d004b8386ca24bd72603658076898467b769e61
Joshua Song <joshua.song@princeton.edu>
2026-04-30 19:25:29 -0400

    miniassembler

--- assignment6/miniassembler.c
+++ assignment6/miniassembler.c
@@ -1,6 +1,6 @@
 /*--------------------------------------------------------------------*/
 /* miniassembler.c                                                    */
-/* Author: Bob Dondero, Donna Gabai                                   */
+/* Author: Joshua (Kimyung) Song, Sergei Kudriavtcev                  */
 /*--------------------------------------------------------------------*/
 
 #include "miniassembler.h"
@@ -20,7 +20,19 @@ static void setField(unsigned int uiSrc, unsigned int uiSrcStartBit,
                      unsigned int uiNumBits)
 {
    /* Your code here */
+   unsigned int uiMask;
+   unsigned int uiField;
 
+   if (uiNumBits == 32) { /* take all bits */
+      uiMask = 0xffffffff;
+   } else { /* shift by uiNumBits and subtract 1 */
+      uiMask = (1 << uiNumBits) - 1;
+   }
+
+   uiField = (uiSrc >> uiSrcStartBit) & uiMask; /* shift and mask source */
+   uiField = uiField << uiDestStartBit; /* shift field to destination */
+
+   *puiDest = *puiDest | uiField; /* set bits in destination to 1 */
 }
 
 /*--------------------------------------------------------------------*/
@@ -28,7 +40,16 @@ static void setField(unsigned int uiSrc, unsigned int uiSrcStartBit,
 unsigned int MiniAssembler_mov(unsigned int uiReg, int iImmed)
 {
    /* Your code here */
+   unsigned int uiInstr;
+   unsigned int uiImmed;
 
+   uiInstr = 0x52800000; /* 10100101 */
+   uiImmed = (unsigned int)iImmed;
+
+   setField(uiImmed, 0, &uiInstr, 5, 16);
+   setField(uiReg, 0, &uiInstr, 0, 5);
+
+   return uiInstr;
 }
 
 /*--------------------------------------------------------------------*/
@@ -60,7 +81,14 @@ unsigned int MiniAssembler_strb(unsigned int uiFromReg,
    unsigned int uiToReg)
 {
    /* Your code here */
+   unsigned int uiInstr;
 
+   uiInstr = 0x39000000; /* 00111001 */
+
+   setField(uiFromReg, 0, &uiInstr, 0, 5);
+   setField(uiToReg, 0, &uiInstr, 5, 5);
+
+   return uiInstr;
 }
 
 /*--------------------------------------------------------------------*/
@@ -69,5 +97,13 @@ unsigned int MiniAssembler_b(unsigned long ulAddr,
    unsigned long ulAddrOfThisInstr)
 {
    /* Your code here */
+   unsigned int uiInstr;
+   unsigned int uiDisp;
 
+   uiInstr = 0x14000000; /* 00010100 */
+   uiDisp = (unsigned int)((ulAddr - ulAddrOfThisInstr) >> 2);
+
+   setField(uiDisp, 0, &uiInstr, 0, 26);
+
+   return uiInstr;
 }
